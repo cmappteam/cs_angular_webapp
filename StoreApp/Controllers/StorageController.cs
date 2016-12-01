1d11a8de96560f1184d0673bff3bdfe14cf77572
rajiv.thota <rajiv.thota.contractor@crossmatch.com>
2016-11-30 23:06:41 -0800

    updated app

---
+++ StoreApp/Controllers/StorageController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+namespace StoreApp.Controllers
+{
+    [Route("api/storage")]
+    public class StorageController : ApiController
+    {
+        private static List<Payload> data = new List<Payload>() { new Payload() { Message = "msg" } };
+
+        [HttpGet]
+        public IEnumerable<Payload> Get()
+        {
+            return data;
+        }
+
+        [HttpPost]
+        public IEnumerable<Payload> Post([FromBody]Payload p)
+        {
+            data.Add(p);
+            return data;
+        }
+    }
+
+    public class Payload
+    {
+        public string Message { get; set; }
+    }
+}
---
+++ StoreApp/Scripts/app/app.js
@@ -0,0 +1,53 @@
+﻿angular.module('app', [])
+    .controller('AppCtrl', AppCtrl)
+    .service('ApiService', ApiService)
+;
+
+AppCtrl.$inject = ['ApiService'];
+function AppCtrl(apiService) {
+    const vm = this;
+    vm.msg = null;
+    vm.saveMsg = function () {
+        apiService.post('api/storage', vm.msg)
+        .then(res => {
+            console.log("res is:", res);
+            vm.messages = res.data;
+        });
+        console.log("saving:", vm.msg);
+    };
+    apiService.get('/api/storage')
+    .then(res => {
+        vm.messages = res.data;
+        console.log('res is:', res.data);
+    });
+}
+
+ApiService.$inject = ['$http'];
+function ApiService($http) {
+    var apiUri = 'http://localhost:35469';
+    this.get = function (url) {
+        return $http({
+            method: 'GET',
+            url: url,
+            headers: {
+                'Accept': "application/json"
+            }
+        })
+        .then(res => res);
+    };
+
+    this.post = function (url, data) {
+        let payload = { message : data };
+        return $http({
+            method: 'POST',
+            url: url,
+            data: angular.toJson(payload),
+            headers: {
+                'Content-Type': "application/json"
+            }
+        })
+        .then(res => {
+            return res;
+        })
+    }
+}
