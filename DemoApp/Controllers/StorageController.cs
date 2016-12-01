2d32dba5b096809fe955243d37dcc916adbadd15
rajiv.thota <rajiv.thota.contractor@crossmatch.com>
2016-11-30 16:59:42 -0800

    initial commit

---
+++ DemoApp/Controllers/StorageController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    [Route("/[controller]")]
+    public class StorageController : Controller
+    {
+        private static List<string> data = new List<string>() { };
+        // GET storage/
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return data;
+        }
+
+        // POST storage
+        [HttpPost]
+        public IEnumerable<string> Post([FromBody]Payload p)
+        {
+            data.Add(p.Data);
+            return data;
+        }
+
+        // PUT storage/5
+        [HttpPut("{val}")]
+        public void Put(string val, [FromBody]string value)
+        {
+            data.Add(val);
+        }
+
+        // DELETE storage/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Payload
+    {
+        public string Data { get; set; }
+    }
+}
---
+++ DemoApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.Extensions.Configuration;
+
+namespace DemoApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var ipHost = Dns.GetHostEntryAsync(Dns.GetHostName()).Result;
+            var ipAddr = ipHost.AddressList.First(a => a.AddressFamily == AddressFamily.InterNetwork);
+
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables(prefix: "ASPNETCORE_")
+                .Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseUrls($"http://127.0.0.1:5000")
+                .Build();
+
+            host.Run();
+        }
+    }
+}
